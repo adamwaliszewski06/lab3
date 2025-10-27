40b6d540324dc12b6ea33423108be56d78604d4c
student <student@LAB-P6-32>
2025-10-27 17:20:00 +0100

    Add project files.

---
+++ lab3/lab3.cpp
@@ -0,0 +1,26 @@
+// lab3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+/* zad 1 
+	int a, b;
+	cout << "Enter two integers: ";
+	cin >> a;
+	cin >> b;
+	if (a==b) {
+		cout << "The numbers are equal." << endl;
+	}
+	else if (a<b) {
+		cout << "The first number is less than the second." << endl;
+	}
+	else {
+		cout << "The first number is greater than the second." << endl;
+	}*/
+
+
+}
+
