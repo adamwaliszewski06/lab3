760492901dfd6ecc2c57d345103fa582a15740a3
adamw06 <adam.waliszewski06@gmail.com>
2025-10-27 17:53:24 +0100

    zad 4

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -39,7 +39,48 @@ int main()
 				cout << "The numbers are equal";
 			} */
 
-			/* zad 3 */
+			/* zad 3 
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			*/
+
+	/* zad 4 */
+	float a, b;
+	char operation;
+
+	cout << "Enter two numbers: ";
+	cin >> a;
+	cin >> b;
+	cout << "Enter the operation (+, -, *, /): ";
+	cin >> operation;
+	switch (operation) {
+	case '+':
+		cout << "Result: " << a + b << endl;
+		break;
+	case '-':
+		cout << "Result: " << a - b << endl;
+		break;
+	case '*':
+		cout << "Result: " << a * b << endl;
+		break;
+	case '/':
+		if (b != 0) {
+			cout << "Result: " << a / b << endl;
+		}
+		else {
+			cout << "Error: Division by zero!" << endl;
+		}
+		break;
+	default:
+		cout << "Invalid operation" << endl;
+	}
 	
 	
 
